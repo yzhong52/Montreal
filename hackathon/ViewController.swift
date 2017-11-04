a165e8c5813ba5728b68f7a0a279572ca871032e
Yuchen Zhong <yuchen@hopper.com>
2017-11-04 12:27:49 -0400

    position the left ball in the scene

--- hackathon/ViewController.swift
+++ hackathon/ViewController.swift
@@ -20,15 +20,26 @@ class ViewController: NSViewController {
         backgroundView.layer?.backgroundColor = NSColor.white.cgColor
     }
     
-
+    
     private let ballSize: CGFloat = 10
     override func viewWillAppear() {
         super.viewWillAppear()
         
         leftBall.wantsLayer = true
         leftBall.layer?.backgroundColor = CGColor.black
+        
+        
+        let constraint = leftBall.topAnchor.constraint(equalTo: view.topAnchor)
+        constraint.constant = 30
+        constraint.isActive = true
+        
+        let leftConstraint = leftBall.leftAnchor.constraint(equalTo: view.leftAnchor)
+        leftConstraint.constant = 30
+        leftConstraint.isActive = true
+        
         leftBall.widthAnchor.constraint(equalToConstant: ballSize).isActive = true
         leftBall.heightAnchor.constraint(equalToConstant: ballSize).isActive = true
+        
         [leftBall].forEach { (aView) in
             if let aView = aView {
                 aView.layer?.cornerRadius = ballSize / 2
