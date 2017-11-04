852eacb498d6bdcf6ff13d7bd5c18ce76f30bce2
Yuchen Zhong <yuchen@hopper.com>
2017-11-04 10:28:21 -0400

    Initial commit

---
+++ hackathon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hackathon
+//
+//  Created by Yuchen Zhong on 2017-11-04.
+//  Copyright © 2017 Yuchen Zhong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hackathon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hackathon
+//
+//  Created by Yuchen Zhong on 2017-11-04.
+//  Copyright © 2017 Yuchen Zhong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
