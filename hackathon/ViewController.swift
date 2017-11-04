8b38ef81089518fa3b204015fbce5e2e328e10f9
Yuchen Zhong <yuchen@hopper.com>
2017-11-04 12:19:22 -0400

    Made the ball a bit smaller

--- hackathon/ViewController.swift
+++ hackathon/ViewController.swift
@@ -9,24 +9,34 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet var backgroundView: NSView!
     @IBOutlet var leftBall: NSView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        leftBall.wantsLayer = true
+        backgroundView.layer?.backgroundColor = NSColor.white.cgColor
+    }
+    
 
+    private let ballSize: CGFloat = 10
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        leftBall.wantsLayer = true
+        leftBall.layer?.backgroundColor = CGColor.black
+        leftBall.widthAnchor.constraint(equalToConstant: ballSize).isActive = true
+        leftBall.heightAnchor.constraint(equalToConstant: ballSize).isActive = true
         [leftBall].forEach { (aView) in
             if let aView = aView {
-                aView.layer?.cornerRadius = aView.frame.width / 2
+                aView.layer?.cornerRadius = ballSize / 2
                 aView.layer?.backgroundColor = CGColor.black
             }
         }
-        
-        backgroundView.layer?.backgroundColor = NSColor.white.cgColor
     }
-
-
-
+    
+    
 }
 
