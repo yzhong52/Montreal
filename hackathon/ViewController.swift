4c257e2e6a491e10d47b033add2650b07c903b72
Yuchen Zhong <yuchen@hopper.com>
2017-11-04 10:54:04 -0400

    connect balls and the ressort tgt

--- hackathon/ViewController.swift
+++ hackathon/ViewController.swift
@@ -10,10 +10,20 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var leftBall: NSView!
+    @IBOutlet var rightBall: NSView!
+    @IBOutlet var springResortView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        [leftBall, rightBall].forEach { (aView) in
+            aView?.layer?.cornerRadius = leftBall.frame.width / 2
+            aView?.layer?.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+        
+        self.springResortView.rotate(byDegrees: 90)
+        self.view.layer?.backgroundColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
     }
 
     override var representedObject: Any? {
